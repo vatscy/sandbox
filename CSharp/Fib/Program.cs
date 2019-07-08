51e2158e487bc015680e06e84820e2ef553604a8
Naoya KOBAYASHI <kobayashi_na@mti.co.jp>
2019-07-08 15:12:43 +0900

    tabify

--- CSharp/Fib/Program.cs
+++ CSharp/Fib/Program.cs
@@ -2,24 +2,24 @@ using System;
 
 namespace Fib
 {
-public class Program
-{
-  public static void Main(string[] args)
-  {
-    var n = long.Parse(Console.ReadLine());
+	public class Program
+	{
+	  public static void Main(string[] args)
+	  {
+	    var n = long.Parse(Console.ReadLine());
 
-    long? result;
-    var time = Measure(() => Fib.Calculate(n).Get(), out result);
+	    long? result;
+	    var time = Measure(() => Fib.Calculate(n).Get(), out result);
 
-    Console.WriteLine(result);
-    Console.WriteLine(time);
-  }
+	    Console.WriteLine(result);
+	    Console.WriteLine(time);
+	  }
 
-  public static TimeSpan Measure<T>(Func<T> func, out T result)
-  {
-    var startTime = DateTime.Now;
-    result = func();
-    return DateTime.Now - startTime;
-  }
-}
+	  public static TimeSpan Measure<T>(Func<T> func, out T result)
+	  {
+	    var startTime = DateTime.Now;
+	    result = func();
+	    return DateTime.Now - startTime;
+	  }
+	}
 }
